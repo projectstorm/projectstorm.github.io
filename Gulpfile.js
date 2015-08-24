1ad9c22d89c8fc6e1bd13bfae21d79a16f379269
Alexis Vincent <alexisjohnvincent@gmail.com>
2015-08-24 15:13:23 +0200

    Version 2

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,17 +3,17 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-	gulp.src('./sass/**/*.scss')
+	gulp.src('./assets/sass/storm.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('./build/'));
 });
 
 gulp.task('sass:watch', function () {
-	gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch('./assets/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass','sass:watch']);
